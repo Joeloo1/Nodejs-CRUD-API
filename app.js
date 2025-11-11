fe08ccf37c6b0279ee445afb278eb386451c7a94
Joel <agboolajoel123@gmail.com>
2025-11-11 08:32:27 +0100

    feat(routes): add middleware to handle unhandled routes with proper error response

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ app.use(express.json());
 
 app.use("/api/v1/products", productRouter);
 
+// HANDLING unhandled routes 
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+})
+
 module.exports = app;
