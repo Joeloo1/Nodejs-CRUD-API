a941ea693861952ea3aeff1cdd1a9edee5d696ab
Joel <agboolajoel123@gmail.com>
2025-11-13 10:40:55 +0100

    feat: add global error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const productRouter = require("./routes/productrouter");
+const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
 
@@ -12,11 +13,13 @@ app.use(express.json());
 app.use("/api/v1/products", productRouter);
 
 // HANDLING unhandled routes 
-app.use('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
     res.status(404).json({
         status: 'fail',
         message: `Can't find ${req.originalUrl} on this server`
     })
-})
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
