a2cde03302c87e190af2cf9e4a0b7f6c87f5a856
Joel <agboolajoel123@gmail.com>
2025-11-14 11:37:59 +0100

    add basic AppError function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 
 const productRouter = require("./routes/productrouter");
 const globalErrorHandler = require('./controller/errorController');
+const AppError = require("./utils/appError");
 
 const app = express();
 
@@ -14,10 +15,7 @@ app.use("/api/v1/products", productRouter);
 
 // HANDLING unhandled routes 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    })
+   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 });
 
 app.use(globalErrorHandler);
