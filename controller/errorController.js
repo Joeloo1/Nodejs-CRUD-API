13472ca6c8d71bb4a2285a4f94bbbe5cedff9b6d
Joel <agboolajoel123@gmail.com>
2025-11-23 01:16:53 +0100

    feat(errors): add database error handlers for cast, duplicate, and validation issues

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400)
+}
+
+const handleDuplicateErrorDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}, Please use another value!`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = (err) => {
+    const errors  = Object.values(err.errors).map((el) => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400)
+}
+
+
 const sendDev = (err, res) => {
     res.status(err.statusCode).json({
             status: err.status,
@@ -33,6 +53,11 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
        sendDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        sendProd(err, res)
+        let error = { ...err };
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.name === 11000) error = handleDuplicateErrorDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
+        sendProd(error, res)
     }
 }
